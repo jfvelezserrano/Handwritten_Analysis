9815fbcdb25f9137649fa36bcdfd1eca33cebc8a
jfvelezserrano <joe.velez@urjc.es>
2021-09-03 14:18:44 +0200

    Preparando test

--- patch_generator_test.py
+++ patch_generator_test.py
@@ -1,7 +1,6 @@
 import time
-
 from genera_palabras_al_azar import genera_palabras_al_azar
 
 start = time.time()
-train_set = complete_set(list_random_strings)
+train_set = complete_set(genera_palabras_al_azar())
 print("Tiempo:", time.time()-start)
